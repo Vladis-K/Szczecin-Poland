4d62a155c4c07cd8a4c96cb5f4fd15bf2cba4e97
Vladys <vladis.kh@gmail.com>
2016-09-04 14:44:48 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,31 @@
 jQuery(function($) {
-  "use strict";
+
+  var w = $('#slider-1').width();
+        $('.all-slides>li').width(w);
+        $('.all-slides').width(w*$('.all-slides>li').length);
+
+        $('.all-slides').css('left', -w);
+        $('.all-slides>li:last-child').prependTo('.all-slides');
+
+        function next(){
+           $('.all-slides').animate({
+               'margin-left': -w
+           }, 1200, function(){
+              $('.all-slides>li:first-child').appendTo('.all-slides');
+              $('.all-slides').css('margin-left', '0');
+           })
+       }
+        $('#nextSlide').click(next);
+
+        function prev(){
+            $('.all-slides').animate({
+                'margin-left': w
+            }, 1200, function(){
+                $('.all-slides>li:last-child').prependTo('.all-slides');
+                $('.all-slides').css('margin-left', '0');
+            })
+        }
+        $('#prevSlide').click(prev);
   
 });
 
