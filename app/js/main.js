c7370a38277c91e1a909e45b86084a43d913a52e
Vladys <vladis.kh@gmail.com>
2016-09-10 17:39:59 +0300

    debug_5

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,6 @@
 jQuery(function($) {
 
-  var w = $('#slider-1').width();
-        $('.all-slides>li').width(w);
-        $('.all-slides').width(w*$('.all-slides>li').length);
 
-        $('.all-slides').css('left', -w);
-        $('.all-slides>li:last-child').prependTo('.all-slides');
-
-        function next(){
-           $('.all-slides').animate({
-               'margin-left': -w
-           }, 1200, function(){
-              $('.all-slides>li:first-child').appendTo('.all-slides');
-              $('.all-slides').css('margin-left', '0');
-           })
-       }
-        $('#nextSlide').click(next);
-
-        function prev(){
-            $('.all-slides').animate({
-                'margin-left': w
-            }, 1200, function(){
-                $('.all-slides>li:last-child').prependTo('.all-slides');
-                $('.all-slides').css('margin-left', '0');
-            })
-        }
-        $('#prevSlide').click(prev);
-  
 });
 
 
